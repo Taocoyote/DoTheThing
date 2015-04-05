677c50210bce6e26a274c041e743acc5f9e8e36d
Glenn Sandoval <glenn.sandoval@gmail.com>
2015-04-05 15:00:57 +1200

    Initial commit

---
+++ app/src/main/java/com/sandoval/glenn/dothething/MainActivity.java
@@ -0,0 +1,70 @@
+package com.sandoval.glenn.dothething;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems = new ArrayList<>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter = 0;
+
+    ListView list1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        list1 = (ListView) findViewById(R.id.list);
+        list1.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonClick(View v) {
+        listItems.add("Clicked : " + clickCounter++);
+        adapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/sandoval/glenn/dothething/TaskAdapter.java
@@ -0,0 +1,21 @@
+package com.sandoval.glenn.dothething;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Glenn on 5/04/2015.
+ */
+public class TaskAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount(){
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View v, ViewGroup vg){
+        return null;
+    }
+}
