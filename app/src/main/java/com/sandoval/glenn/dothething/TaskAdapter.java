2b1fe7e85b4d6a8bda9f9d09d57d20910ea4e4ac
Glenn Sandoval <glenn.sandoval@gmail.com>
2015-04-06 13:26:50 +1200

    Make TaskAdapter compile

--- app/src/main/java/com/sandoval/glenn/dothething/TaskAdapter.java
+++ app/src/main/java/com/sandoval/glenn/dothething/TaskAdapter.java
@@ -5,17 +5,32 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 /**
+ * Confounds your enemies and amazes your friends
  * Created by Glenn on 5/04/2015.
  */
 public class TaskAdapter extends BaseAdapter {
 
+    public TaskAdapter(int i) {
+        super();
+    }
+
     @Override
-    public int getCount(){
+    public int getCount() {
         return 0;
     }
 
     @Override
-    public View getView(int i, View v, ViewGroup vg){
+    public View getView(int i, View v, ViewGroup vg) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 1;
+    }
+
+    @Override
+    public Object getItem(int position) {
         return null;
     }
 }
