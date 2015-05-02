568705b80b63bf12ed66dd5e967cf00f2c6c6ae2
Glenn Sandoval <glenn.sandoval@gmail.com>
2015-05-02 15:31:24 +1200

    Added ability to select an image

--- app/src/main/java/com/sandoval/glenn/dothething/CreateTaskActivity.java
+++ app/src/main/java/com/sandoval/glenn/dothething/CreateTaskActivity.java
@@ -1,11 +1,23 @@
 package com.sandoval.glenn.dothething;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 
 public class CreateTaskActivity extends Activity {
+
+    private Uri selectedImage;
+    static final int SELECT_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,4 +39,69 @@ public class CreateTaskActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void buttonOkClick(View v) {
+        finish();
+    }
+
+    public void buttonCancelClick(View v) {
+        finish();
+    }
+
+    public void selectImage(View v) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    selectedImage = imageReturnedIntent.getData();
+                    try {
+                        decodeUri(selectedImage);
+                    } catch (FileNotFoundException fnf){
+                        //panic
+                    }
+                }
+        }
+    }
+
+    private void decodeUri(Uri selectedImage) throws FileNotFoundException {
+
+        // Decode image size
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o);
+
+        // The new size we want to scale to
+        final int REQUIRED_SIZE = 64;
+
+        // Find the correct scale value. It should be the power of 2.
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+        while (true) {
+            if (width_tmp / 2 < REQUIRED_SIZE
+                    || height_tmp / 2 < REQUIRED_SIZE) {
+                break;
+            }
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        // Decode with inSampleSize
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        Bitmap bm = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o2);
+
+        ImageView iv = (ImageView)findViewById(R.id.taskImageView);
+       iv.setImageBitmap(bm);
+
+    }
+
 }
